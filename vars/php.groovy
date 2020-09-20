c962eb29192f98b0b803f81140f9205539dd0c2f
Amit Chauhan <er.amitkchauhan@gmail.com>
2020-09-20 16:30:26 -0400

    Job

---
+++ vars/php.groovy
@@ -0,0 +1,25 @@
+def call() {
+
+    pipeline {
+        agent any
+
+
+        stages {
+
+
+
+            stage('Lint Checking code') {
+                steps {
+                    sh '''
+                            composer require overtrue/phplint --dev -vvv
+                            ./vendor/bin/phplint html
+                        '''
+
+                }
+
+            }
+
+
+        }
+    }
+}
