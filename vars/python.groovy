9d7bb53c0e137f57053b20e9e66f288f9472d3f0
Amit Chauhan <er.amitkchauhan@gmail.com>
2020-09-20 16:27:35 -0400

    Job

---
+++ vars/python.groovy
@@ -0,0 +1,22 @@
+def call() {
+
+    pipeline {
+        agent any
+
+
+        stages {
+            stage('Python Lint Check') {
+                steps {
+                    sh '''
+                    pylint payment.py || true
+                    pylint rabbitmq.py || true
+                '''
+                }
+
+            }
+
+
+        }
+
+    }
+}
